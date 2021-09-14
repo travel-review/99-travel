2f1f967b26837485f608a9c0281ba80c2d23c34b
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-09-14 10:09:44 +0900

    로그인, 회원가입 ing 임시

--- app.py
+++ app.py
@@ -1,61 +1,74 @@
-from flask import Flask, request, jsonify, make_response, render_template, session
+from flask import Flask, request, jsonify, redirect, url_for, make_response, render_template, session
 from datetime import datetime, timedelta
 from functools import wraps
+from pymongo import MongoClient
+import jwt
 
 app = Flask(__name__)
 
-'''
-app.config['SECRET_KEY']= 'hanghae'
-def token_required(func):
-    @wraps(func)
-    def decorated(*args, **kwargs):
-        token = request/args.get('token')
-        if not token:
-            return json({'토큰이 없습니다!'})
-        try:
-            payload = jwt.decode(token, app.config['SECRET_KEY'])
-        except:
-            return json({'사용불가능한 토큰입니다.!'})
-    return decorated
+client = MongoClient('127.0.0.1')
+db = client.my_sparta
 
-def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return '이미 로그인 되어있습니다.'
-    
-@app.route('/puiblic')
-def public():
-    return 'for public'
-
-@app.route('/auth')
-@token_required
-def aut():
-    return ' 로그인 완료.'
-
-@app.route('/login', methods =['POST'])
-def login():
-    if request.form['username'] and request.form['password'] =='12345':
-        session['logged_in'] = True
-        token = jwt.encode({
-            'user' : request.form['username'],
-            'expiration': datetime.datetime.utcnow() + datetime.timedelta(seconds=120)
-        },
-         app.config['SECRET_KEY'])
-        return jsonify({'token':token.decode('utf-8')})
-
-    else:
-        return make_respose('Unable to verify', 403,{'WWW-Authenticate' : 'Basic realm:"Authentication Failed'})
-'''
-
+SECRET_KEY = 'SPARTA'
 
 @app.route('/')
 def login():
-    return render_template('login.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})
+        print(user_info)
+        return render_template('login.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/api/signin', methods=['POST'])
+def api_signin():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    pw_hash = pw_receive
+
+    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
+    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if result is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
+        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+        payload = {
+            'id': id_receive,
+        }
+        token = jwt.encode(payload, SECRET_KEY,
+                           algorithm='HS256')
+
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@app.route('/api/signup', methods=['POST'])
+def api_signup():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    pw_hash = pw_receive
+
+    db.user.insert_one(
+        {'id': id_receive, 'pw': pw_hash})
+
+    return jsonify({'result': 'success'})
 
 @app.route('/landing')
 def landing():

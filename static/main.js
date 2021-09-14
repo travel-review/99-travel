9a6b82eb6c5cd2c711ddb81341c71a40cb289e4f
covy92 <tjddlf1232@gmail.com>
2021-09-14 09:45:23 +0900

    add mainpage

---
+++ static/main.js
@@ -0,0 +1,16 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function(){
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder','검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder','');
+});
